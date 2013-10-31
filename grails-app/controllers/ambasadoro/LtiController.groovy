2f683799dcce84200cff5884bdc7545baa14bd26
Jesus Federico <jesus@123it.ca>
2013-10-31 00:35:39 -0400

    first commit

---
+++ grails-app/controllers/ambasadoro/LtiController.groovy
@@ -0,0 +1,13 @@
+package ambasadoro
+
+class LtiController {
+
+    def index() { }
+	
+	def tool() { }
+	
+	def tool_cartridge() { }
+	
+	def tool_ui() { }
+	
+}
---
+++ test/unit/ambasadoro/LtiControllerTests.groovy
@@ -0,0 +1,17 @@
+package ambasadoro
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(LtiController)
+class LtiControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
