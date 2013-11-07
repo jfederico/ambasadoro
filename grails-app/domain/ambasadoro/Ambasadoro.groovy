c5b2f27040b4bd63001043e4cbc205aac233398f
jfederico <jesus@123it.ca>
2013-11-07 11:05:22 -0800

    Added json library and redefined the current model

--- grails-app/domain/ambasadoro/Ambasadoro.groovy
+++ grails-app/domain/ambasadoro/Ambasadoro.groovy
@@ -1,14 +1,15 @@
 package ambasadoro
 
-class Entity {
+class Ambasadoro {
     // Auto Timestamp
     Date dateCreated
     Date lastUpdated
 
     String  lti_key
     String  lti_secret
-    String  tc_type
-    String  tp_type
+    String  tool_code
+    String  tcool_consumer_code
+    String  meta
 
     static constraints = {
     }
--- test/unit/ambasadoro/AmbasadoroTests.groovy
+++ test/unit/ambasadoro/AmbasadoroTests.groovy
@@ -8,8 +8,8 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Entity)
-class EntityTests {
+@TestFor(Ambasadoro)
+class AmbasadoroTests {
 
     void testSomething() {
        fail "Implement me"
