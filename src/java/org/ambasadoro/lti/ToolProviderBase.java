d425f158d1b08ecb85f11419f850b00325e935a7
Jesus Federico <jesus@123it.ca>
2013-11-10 21:39:18 -0500

    Removed unnecesary code from ToolProviderBase

--- src/java/org/ambasadoro/lti/ToolProviderBase.java
+++ src/java/org/ambasadoro/lti/ToolProviderBase.java
@@ -5,7 +5,6 @@ import java.util.Properties;
 
 import net.oauth.OAuth;
 import net.oauth.OAuthMessage;
-import net.oauth.signature.OAuthSignatureMethod;
 import net.oauth.signature.HMAC_SHA1;
 
 public class ToolProviderBase implements IToolProvider {
@@ -64,32 +63,4 @@ public class ToolProviderBase implements IToolProvider {
 
         return reqProp;
     }
-
-
-    /*
-    private boolean hasValidSignature(Customer customer, Object params) {
-        boolean validSignature = false
-
-        if (params.containsKey(OAuth.OAUTH_SIGNATURE)) {
-            String signature = params.get(OAuth.OAUTH_SIGNATURE)
-            String method = request.getMethod().toUpperCase()
-            String URL = epcServerService.retrieveBasicLtiEndpoint(request.isSecure()?"https":"http")
-            String conSecret = customer.sharedSecret
-            Object postProp = sanitizePrametersForBaseString(params)
-            
-            OAuthMessage oam = new OAuthMessage(method, URL, ((Properties)postProp).entrySet())
-            HMAC_SHA1 hmac = new HMAC_SHA1()
-            hmac.setConsumerSecret(conSecret)
-
-            log.debug("Base Message String = [ " + hmac.getBaseString(oam) + " ]\n")
-            String calculatedSignature = hmac.getSignature(hmac.getBaseString(oam))
-            log.debug("Calculated: " + calculatedSignature + " Received: " + signature)
-            validSignature = calculatedSignature.equals(signature)
-
-        }
-
-        return validSignature
-    }
-    */
-
 }
