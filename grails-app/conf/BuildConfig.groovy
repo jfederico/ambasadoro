a987df3a385ca2af70d170d02ca90c7558fe99d5
Jesus Federico <jesus@123it.ca>
2014-08-20 20:10:18 -0400

    Added plugin ambasadoro-plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -69,8 +69,9 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.50.1"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.2"
+        compile ':scaffolding:2.0.2'
         compile ':cache:1.1.1'
+		compile ':ambasadoro-plugin:0.1'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"
