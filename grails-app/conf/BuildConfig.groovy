3e41dbbd7182ebca0b04cad8f510fe0e4c49718b
Jesus Federico <jesus@123it.ca>
2014-08-30 13:51:05 -0400

    For development only, added ambasadoro-plugin and ambasadoro-plugin-bigbluebutton libraries (They should be removed after)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -71,9 +71,9 @@ grails.project.dependency.resolution = {
         compile ':scaffolding:2.0.2'
         compile ':cache:1.1.1'
 
-        compile ':ambasadoro-plugin:0.4'
-        compile ':ambasadoro-plugin-test:0.1'
-        compile ':ambasadoro-plugin-bigbluebutton:0.3'
+        //compile ':ambasadoro-plugin:0.5'
+        //compile ':ambasadoro-plugin-test:0.1'
+        //compile ':ambasadoro-plugin-bigbluebutton:0.5'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"
